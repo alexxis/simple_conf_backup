c3f0299d8507799594cfa75fd1226c532c05d834
alexxis <starikoff@mail.ru>
2015-02-14 18:04:22 +0300

    Update conf_backup.sh

--- conf_backup.sh
+++ conf_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -czv -T ./include -X ./exclude -f ~punisher/backup/conf_nau_`date '+%Y-%m-%d_%H-%M-%S'`.tar.gz
+tar -czv -T ./conf_backup_include -X ./conf_backup_exclude -f ~punisher/backup/conf_nau_`date '+%Y-%m-%d_%H-%M-%S'`.tar.gz
